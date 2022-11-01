1d702871ae6d6661a72a8bd0ce8541a0775b75be
z9fr <dasith@surge.global>
2022-11-01 22:04:28 +0530

    fix: lib limit issue

--- src/hooks/library/useBooks.tsx
+++ src/hooks/library/useBooks.tsx
@@ -2,7 +2,9 @@ import httpConfig from "../../utils/request";
 import { useQuery } from "@tanstack/react-query";
 
 const fetchData = (category: string) =>
-  httpConfig.get(`meta/library/${category}`).then((response) => response.data);
+  httpConfig
+    .get(`meta/library/${category}?limit=100`)
+    .then((response) => response.data);
 
 export function useBooks(category: string) {
   return useQuery(["books", category], () => fetchData(category));
