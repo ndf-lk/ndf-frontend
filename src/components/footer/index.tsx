20706dea56d93e092c0e2b17c1eea17b969a092d
z9fr <z9fr@protonmail.com>
2022-11-27 10:15:28 +0530

    feat: updated footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -11,6 +11,7 @@ import { getData } from "../../data/footer";
 import { useContext } from "react";
 import { styled } from "@mui/material/styles";
 import { LanguageContext } from "../../context/userLangctx";
+import { Link } from "react-router-dom";
 
 const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
   color: theme.palette.getContrastText("#FFFFFF"),
@@ -32,55 +33,93 @@ const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <Box
-          sx={{
-            borderRadius: 0,
-            margin: 0,
-          }}
+        <Stack
+          justifyContent="space-around"
+          alignItems="center"
+          direction={{ xs: "column", sm: "row" }}
+          spacing={{ xs: 1, sm: 2, md: 4 }}
         >
-          <Box>
+          <Box style={{ float: "left", padding: 20 }}>
             <Box
               component="img"
-              className="footer_main_img"
-              textAlign="center"
               alt="test"
               src="/logo-home.png"
               width={{ xs: 100, md: 150 }}
             />
 
-            <Typography
-              variant={"h4"}
-              textAlign="center"
-              sx={{ color: "white" }}
-            >
+            <Typography variant={"h4"} sx={{ color: "white" }}>
               {content.main.title}
             </Typography>
 
-            <Typography
-              variant={"body1"}
-              textAlign="center"
-              sx={{ color: "white" }}
-            >
+            <Typography variant={"body1"} sx={{ color: "white" }}>
               {content.main.secondary}
             </Typography>
 
-            <Box textAlign="center" sx={{ mt: 5 }}>
-              <ColorButton> {content.party_membership_btn} </ColorButton>
+            <Box textAlign="left" sx={{ mt: 5 }}>
+              {/*@ts-ignore */}
+              <ColorButton size="small" component={Link} to="/register">
+                {content.party_membership_btn}
+              </ColorButton>
             </Box>
-
-            <Typography
-              variant={"body1"}
-              textAlign="center"
-              className="footer_description"
-              mt={{ xs: 5, md: 5 }}
-              sx={{
-                color: "white",
-              }}
-            >
-              {content.description}
-            </Typography>
           </Box>
-        </Box>
+
+          <Box style={{ float: "left", padding: 20 }}>
+            <Typography variant={"h4"} sx={{ color: "white" }}>
+              Langauges
+            </Typography>
+
+            <Stack sx={{ mt: 10 }} style={{ textAlign: "left" }}>
+              <Typography
+                style={{ color: "white", textDecoration: "none" }}
+                sx={{ mt: 5 }}
+              >
+                සිංහල
+              </Typography>
+
+              <Typography
+                style={{ color: "white", textDecoration: "none" }}
+                sx={{ mt: 5 }}
+              >
+                English
+              </Typography>
+
+              <Typography
+                style={{ color: "white", textDecoration: "none" }}
+                sx={{ mt: 5 }}
+              >
+                தமிழ்
+              </Typography>
+            </Stack>
+          </Box>
+
+          {content.links.map((footernav) => {
+            return (
+              <>
+                <Box style={{ float: "left", padding: 20, textAlign: "left" }}>
+                  <Typography variant={"h4"} sx={{ color: "white" }}>
+                    {footernav.title}
+                  </Typography>
+                  <Stack sx={{ mt: 10 }} style={{ textAlign: "left" }}>
+                    {footernav.links.map((links) => {
+                      return (
+                        <>
+                          <Typography
+                            style={{ color: "white", textDecoration: "none" }}
+                            sx={{ mt: 5 }}
+                            component={Link}
+                            to={`${links.link}`}
+                          >
+                            {links.label}{" "}
+                          </Typography>
+                        </>
+                      );
+                    })}
+                  </Stack>
+                </Box>
+              </>
+            );
+          })}
+        </Stack>
       </footer>
     </>
   );
